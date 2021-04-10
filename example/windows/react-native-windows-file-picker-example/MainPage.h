6ddb68a913911388fe901b84cf6282d20cab84aa
Petr Janik <peta.janik@email.cz>
2021-04-10 04:23:25 -0700

    feat: init windows in example app

---
+++ example/windows/react-native-windows-file-picker-example/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::react-native-windows-file-picker-example::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::react-native-windows-file-picker-example::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
